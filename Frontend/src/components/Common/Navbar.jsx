6ff42b6522ce0e52b8ae4567b9b376e5ff7f3296
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-13 23:59:23 +0545

    Close functionality was added in the searchbar and cart.

--- Frontend/src/components/Common/Navbar.jsx
+++ Frontend/src/components/Common/Navbar.jsx
@@ -5,17 +5,11 @@ import CartDrawer from '../Layout/CartDrawer'
 import { useState } from 'react'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
 
-    const [drawerOpen, setDrawerOpen] = useState(false)
-    const [navDrawerOpen, setNavDrawerOpen] = useState(false)
-
-    const toggleNavDrawer = () => {
-        setNavDrawerOpen(!navDrawerOpen)
+    const handleCart = () => {
+        setIsOpen(!isOpen)
     }
-    
-    const toggleCartDrawer = () => {
-        setDrawerOpen(!drawerOpen)
-    };
 
     return (
     <>
@@ -39,29 +33,26 @@ const Navbar = () => {
                 <Link to="/profile">
                     <BiUser className='h-6 w-6 text-white hover:bg-black hover:rounded-full hover:text-[#CB2957] transition-all ease-in-out duration-300'/>
                 </Link>
-                    <button
-                        className="relative"
-                        onClick={toggleCartDrawer}
-                    >
-                    <BiCart className='h-6 w-6 text-white hover:bg-black hover:rounded-full hover:text-[#CB2957] transition-all ease-in-out duration-300' />
+                <button className="relative">
+                    <BiCart onClick={handleCart} className='h-6 w-6 text-white hover:bg-black hover:rounded-full hover:text-[#CB2957] transition-all ease-in-out duration-300' />
                     <span className="absolute bg-[#CB2957] text-black rounded-full -top-2 text-xs px-1.5 py-px">
                         3
                     </span>
                 </button>
 
-                <SearchBar />
+                <SearchBar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-                <button className="md:hidden" onClick={toggleNavDrawer}>
+                <button className="md:hidden">
                     <BiMenu className='h-6 w-6 text-white hover:text-[#CB2957] transition-all ease-in-out duration-300 ' />
                 </button>
             </div>
             </nav>
             
-            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+            <CartDrawer isOpen={isOpen} setIsOpen={setIsOpen} />
 
-            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-black transform transition-transform duration-200 z-50 ${navDrawerOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            <div className='fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-black transform transition-transform duration-200 z-50 -translate-x-full'>
                 <div className="flex justify-end p-4">
-                    <button onClick={toggleNavDrawer}>
+                    <button>
                         <BiPlus
                             className="h-6 w-6 rotate-135 text-white cursor-pointer hover:text-[#CB2957] hover:rotate-45 transition-all ease-in-out duration-300"
                         />
@@ -70,9 +61,9 @@ const Navbar = () => {
                 <div className="p-4">
                     <h2 className="text-[#CB2957] text-3xl font-semibold mb-4 p-3">Menu</h2>
                     <nav className='space-y-4 px-3'>
-                        <Link to='/collections/guitars' onClick={toggleNavDrawer} className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>Guitars</Link>
-                        <Link to='/collections/amp' onClick={toggleNavDrawer} className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>AMP</Link>
-                        <Link to='#' onClick={toggleNavDrawer} className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>Accessories</Link>
+                        <Link to='/collections/guitars' className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>Guitars</Link>
+                        <Link to='/collections/amp' className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>AMP</Link>
+                        <Link to='#' className='block text-white text-2xl hover:text-[#CB2957] transition-all ease-in-out duration-300'>Accessories</Link>
                     </nav>
                 </div>
             </div>
